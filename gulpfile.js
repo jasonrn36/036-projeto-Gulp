337ec5042db15d493cd95f421cd83f85d2bd3b9b
Jason Ramalho Nascimento <jason.rn36@gmail.com>
2025-06-18 10:14:07 -0400

    Criando outra tarefa em GULP além da tarefa Default

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 function funcaoPadrao(callback) {
-    console.log("Executando via \x1b[31mGULP\x1b[37m");
+    console.log("\n Executando via \x1b[31mGULP\x1b[37m \n");
     callback();
 }
 
+function digaOi(callback) {
+    console.log("\n\x1b[33m O\x1b[36ml\x1b[33má\x1b[31m \x1b[31mGulp\x1b[34m!! \n");
+    callback();
+}
 exports.default = funcaoPadrao;
+exports.digaOi = digaOi;
